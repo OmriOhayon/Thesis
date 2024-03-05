5da546907b9fed4cb73e2d4033fe65ca22597917
OmriOhayon <64261669+OmriOhayon@users.noreply.github.com>
2024-03-05 22:51:39 +0200

    Update Go1_KinematicWalker_Classes_v_0_08_Annealing_clip.py
    Updated runs folder created by SummaryWriter to be created on the remote machine.

--- Go1_KinematicWalker_Classes_v_0_08_Annealing_clip.py
+++ Go1_KinematicWalker_Classes_v_0_08_Annealing_clip.py
@@ -1751,7 +1751,7 @@ if __name__ == "__main__":
             monitor_gym=True,
             save_code=True,
         )
-    writer = SummaryWriter(f"runs\\{run_name}")
+    writer = SummaryWriter(f"runs/{run_name}")
     writer.add_text(
         "hyperparameters",
         "|param|value|\n|-|-|\n%s" % ("\n".join([f"|{key}|{value}|" for key, value in vars(args).items()])),
